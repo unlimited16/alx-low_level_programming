4f2ba430450d1153f539d330dcbad5274b1cc73a
Bakare Opeyemi <bakareopeyemi1722@gmail.com>
2022-12-14 07:33:41 -0800

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints the first fibonacci numbers,
+ * starting with 1 and 2
+ * Return: something
+ */
+int main(void)
+{
+	long int i, j, k, l;
+
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+
+		l = j + k;
+		j = k;
+		k = l;
+	}
+	return (0);
+}
+
