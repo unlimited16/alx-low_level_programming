62a17b542eccf84edf35a901c6733a1531c63b78
Bakare Opeyemi <bakareopeyemi1722@gmail.com>
2023-01-04 13:17:15 -0800

    Added task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,7 +3,7 @@
 /**
  * _strlen_recursion - returns the length of a string.
  * @s: string
- * Return: length of a string
+ * Return: the length of a string
  */
 
 int _strlen_recursion(char *s)
@@ -27,7 +27,7 @@ int comparator(char *s, int n1, int n2)
 	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
-			return (0);
+			return (1);
 		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
