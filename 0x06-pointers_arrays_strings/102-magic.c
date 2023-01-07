dfd42d5879cd84de29536740cd70261eba67fe51
Bakare Opeyemi <bakareopeyemi1722@gmail.com>
2023-01-06 17:23:53 -0800

    Added task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your code here...
+	 * write your line of code here...
 	 * Remeber:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
