8f3c92f82eb35c75c78020eb735df59c8c7a8aff
Bakare Opeyemi <bakareopeyemi1722@gmail.com>
2023-01-06 17:12:30 -0800

    Added task

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -11,4 +13,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
